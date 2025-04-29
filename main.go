cbcc32a4ea359302eeb25ed90ded9ed264b01292
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-04-29 13:36:41 +0700

    function handler

--- main.go
+++ main.go
@@ -17,16 +17,38 @@ type Roll struct {
 // Init Rolls var as a slice
 var rolls []Roll
 
+// Index 
+func getRolls(w http.ResponseWriter, r *http.Request)  {
+	
+}
+// Show Single Sushi 
+func getRoll(w http.ResponseWriter, r *http.Request)  {
+	
+}
+// add Single Sushi 
+func createRoll(w http.ResponseWriter, r *http.Request)  {
+	
+}
+
+// update Single Sushi 
+func updateRoll(w http.ResponseWriter, r *http.Request)  {
+	
+}
+// delete Single Sushi 
+func deleteRoll(w http.ResponseWriter, r *http.Request)  {
+	
+}
+
 func main() {
 	// Init Router
 	router := mux.NewRouter()
 
 	// Handle Endpoint/Routing
-	router.HandleFunc("/sushi").Methods("GET")
-	router.HandleFunc("/sushi/{id}").Methods("GET")
-	router.HandleFunc("/sushi").Methods("POST")
-	router.HandleFunc("/sushi/{id}").Methods("POST")
-	router.HandleFunc("/sushi/{id}").Methods("DELETE")
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	router.HandleFunc("/sushi/{id}", getRoll).Methods("GET")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", deleteRoll).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
